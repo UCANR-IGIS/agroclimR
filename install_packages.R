e89ff221d45d60a61985a6bcf38a782302e4ea37
Andy Lyons <lyons.andy@gmail.com>
2022-11-30 11:11:21 -0800

    Update to index.html

--- install_packages.R
+++ install_packages.R
@@ -1,19 +1,19 @@
-## Required packages
+## Required packages from CRAN
 pkgs_req <- c("sf", "tidyverse", "tmap", "units", "zoo", "remotes", "degday", "chillR", "cimir", "here")
 
 ## See which ones are missing
 (pkgs_missing <- pkgs_req[!(pkgs_req %in% installed.packages()[,"Package"])])
 
-## Install missing ones
+## Install missing CRAN packages
 if (length(pkgs_missing)) install.packages(pkgs_missing, dependencies=TRUE)
 
-if (!"wrkshputils" %in% installed.packages()[,"Package"]) remotes::install_github("ucanr-igis/wrkshputils")
-
-if (!"caladaptr" %in% installed.packages()[,"Package"]) remotes::install_github("ucanr-igis/caladaptr")
-
-## Re-run the check for missing packages
+## Re-run the check for missing CRAN packages
 pkgs_missing <- pkgs_req[!(pkgs_req %in% installed.packages()[,"Package"])]
-if (length(pkgs_missing)==0) cat("ALL PACKAGES WERE INSTALLED SUCCESSFULLY \n")
+if (length(pkgs_missing)==0) cat("ALL CRAN PACKAGES WERE INSTALLED SUCCESSFULLY \n")
+
+## Install two packages from GitHub:
+if (!"wrkshputils" %in% installed.packages()[,"Package"]) remotes::install_github("ucanr-igis/wrkshputils")
+if (!"caladaptr" %in% installed.packages()[,"Package"]) remotes::install_github("ucanr-igis/caladaptr")
 
 #########################################################################################
 ## TIPS
